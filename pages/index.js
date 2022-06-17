e6bfbc44ccf81b7002db559cc7942a8ec4a5207f
Sead135 <optimist53197@gmail.com>
2022-06-17 17:15:56 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 const Home = () => {
   return (
     <Layout>
-      <Typography>Hello</Typography>
+      <Typography>Update</Typography>
     </Layout>
   );
 };
